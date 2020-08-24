d755cf1e967a3170c0035aeaa875157400e2c87c
Jonathan Cantor <jonatcantor@gmail.com>
2020-08-24 19:11:59 -0400

    added player rotation to walls

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    bool forward, right, left;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        //Si el raycast hacia adelande del jugador detecta una pared
+        if(Physics.Raycast(transform.position, transform.forward, out hit)){
+            //Hace que la posición del jugador sea igual al de esa pared detectada
+            //manteniendo su posición en y por si salta en plena transición
+            transform.position = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+        }
+    }
+}
