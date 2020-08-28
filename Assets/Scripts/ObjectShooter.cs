9af6fb6a7407d0384892d8c86e008b83839b6ebf
Jonathan Cantor <jonatcantor@gmail.com>
2020-08-28 15:25:21 -0400

    added object shooter

---
+++ Assets/Scripts/ObjectShooter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectShooter : MonoBehaviour
+{
+    public float speedAttack;
+
+    public GameObject[] objects;
+
+    public Transform target;
+
+    public Transform camera;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(5f);
+
+        while(true){
+            Instantiate(objects[Random.Range(0, objects.Length)], transform.position, transform.rotation);
+            yield return new WaitForSeconds(speedAttack);
+        }
+    }
+
+    void Update(){
+        transform.position = camera.position + Vector3.up * -2f;
+        transform.LookAt(target.position);
+    }
+}
